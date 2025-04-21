70ba9e55746f4f005b68e52b33e3506b705bfb70
ArdianDragusha <ardian.dragusha@myy.haaga-helia.fi>
2025-04-21 15:34:07 +0300

    Changes again

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/k25Tiimi4FrontEnd/'
+  base: '/k25Tiimi4FrontEnd/',
+  server: {
+    port: 5173
+  }
 })
