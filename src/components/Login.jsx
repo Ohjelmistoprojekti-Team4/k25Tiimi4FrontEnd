6d4c5aedae391ba5bb9f89c46c9ba562343e50fa
bhm565@myy.haaga.helia.fi <bhm565@myy.haaga.helia.fi>
2025-04-28 13:29:19 +0300

    Sumbitting of new user

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,46 +2,108 @@ import React, { useState } from "react";
 import './Login.css';
 import { Typography } from "@mui/material";
 import Footer from "./Footer";
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
 
-    const [action, setAction] = useState("Sign Up")
+    const [user, setValues] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        username: '',
+        password: '',
+    });
+    const [action, setAction] = useState("Sign Up");
+
+    const [message, setMessage] = useState('');
+    const navigate = useNavigate();
+
+    const updateUser = (e) => {
+        setValues({
+            ...user,
+            [e.target.name]: e.target.value
+        });
+        setMessage('');
+    }
+
+    const submitNewUser = () => {
+        const { firstName, lastName, email, username, password } = user;
+
+        if (!firstName || !lastName || !email || !username || !password) {
+            setMessage('You should input a value in each field')
+        } else {
+            setMessage('User created');
+            setValues({
+                firstName: '',
+                lastName: '',
+                email: '',
+                username: '',
+                password: '',
+            });
+        }
+    }
+
+    const handleLogin = () => {
+        navigate('/products');
+    }
 
     return (
         <div className="login-page">
-        <div className="container">
-            <div className="header">
-                <Typography variant="h2" className="text">{action}</Typography>
-                <div className="underline"></div>
-            </div>
-            <div>
-                <div className="inputs">
-                    {action === "Sign Up" && (
-                        <>
-                            <div className="input">
-                                <input type="text" placeholder="First name" />
-                            </div>
-                            <div className="input">
-                                <input type="text" placeholder="Last name" />
-                            </div>
-                        </>
-                    )}
-                    <div className="input">
-                        <input type="email" placeholder="Email" />
-                    </div>
-                    <div className="input">
-                        <input type="password" placeholder="Password" />
-                    </div>
+            <div className="container">
+                <div className="header">
+                    <Typography variant="h2" className="text">{action}</Typography>
+                    <div className="underline"></div>
+                </div>
+                <div>
+                    <div className="inputs">
+                        {action === "Sign Up" && (
+                            <>
+                                <div className="input">
+                                    <input type="text" name="firstName" placeholder="First name" value={user.firstName} onChange={(e) => updateUser(e)} />
+                                </div>
+                                <div className="input">
+                                    <input type="text" name="lastName" placeholder="Last name" value={user.lastName} onChange={(e) => updateUser(e)} />
+                                </div>
+                                <div className="input">
+                                    <input type="email" name="email" placeholder="Email" value={user.email} onChange={(e) => updateUser(e)} />
+                                </div>
+                            </>
+                        )}
+                        <div className="input">
+                            <input type="text" name="username" placeholder="Username" value={user.username} onChange={(e) => updateUser(e)} />
+                        </div>
+                        <div className="input">
+                            <input type="password" name="password" placeholder="Password" value={user.password} onChange={(e) => updateUser(e)} />
+                        </div>
 
-                    <div className="submit-container">
-                        <div className={action === "Login" ? "submit gray" : "submit"} onClick={() => { setAction("Sign Up") }}>Sign Up</div>
-                        <div className={action === "Sign Up" ? "submit gray" : "submit"} onClick={() => { setAction("Login") }}>Login</div>
+                        <div className="submit-container">
+                            <div className={action === "Login" ? "submit gray" : "submit"}
+                                onClick={() => {
+                                    if (action === "Sign Up") {
+                                        submitNewUser();
+                                    } else {
+                                        setAction("Sign Up")
+                                    }
+                                }}
+                            >Sign Up
+                            </div>
+                            <div className={action === "Sign Up" ? "submit gray" : "submit"}
+                                onClick={() => {
+                                    if (action === "Login") {
+                                        handleLogin();
+                                    } else {
+                                        setAction("Login")
+                                    }
+                                }}
+                            >Login
+                            </div>
+                        </div>
+                        {message}
                     </div>
                 </div>
             </div>
-        </div>
 
-        <Footer />
+            <Footer />
         </div>
     )
 }
